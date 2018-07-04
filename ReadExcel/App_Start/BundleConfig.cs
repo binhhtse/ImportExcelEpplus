5075c5325cc0434f081cb359cd6f4651043fb665
binh <binhhtse@gmail.com>
2018-07-04 11:38:20 +0700

    Update script

--- ReadExcel/App_Start/BundleConfig.cs
+++ ReadExcel/App_Start/BundleConfig.cs
@@ -8,11 +8,13 @@ namespace ReadExcel
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.dataTables.min.js"
+                        ));
 
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //            "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -25,6 +27,7 @@ namespace ReadExcel
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                       "~/Content/jquery.dataTables.min.css",
                       "~/Content/site.css"));
         }
     }
