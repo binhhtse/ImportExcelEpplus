b19f67420a0ef4e46251b0670226ee1f3f07c6d0
binh <binhhtse@gmail.com>
2018-07-04 11:09:48 +0700

    Init

---
+++ ReadExcel/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using OfficeOpenXml;
+using ReadExcel.Models;
+using System.Data;
+
+namespace ReadExcel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult ReadExcelUsingEpplus()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ReadExcel(HttpPostedFileBase upload)
+        {
+            //if (Path.GetExtension(upload.FileName) == ".xlsx" || Path.GetExtension(upload.FileName) == ".xls")
+            //{
+                ExcelPackage package = new ExcelPackage(upload.InputStream);
+                DataTable Dt = ExcelPackageExtensions.ToDataTable(package);
+                // Getting those error because we don't  have any ReadExcel view . you can use this datatatable where you need it.
+                return View(Dt);
+            //}
+            //return View();
+        }
+    }
+}
---
+++ ReadExcel/Models/ExcelPackageExtensions.cs
@@ -0,0 +1,33 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace ReadExcel.Models
+{
+    public static class ExcelPackageExtensions
+    {
+        public static DataTable ToDataTable(this ExcelPackage package)
+        {
+            ExcelWorksheet workSheet = package.Workbook.Worksheets.First();
+            DataTable Dt = new DataTable();
+            foreach (var firstRowCell in workSheet.Cells[1,1,1,workSheet.Dimension.End.Column])
+            {
+                Dt.Columns.Add(firstRowCell.Text);
+            }
+            for (var rowNumber = 2; rowNumber <= workSheet.Dimension.End.Row; rowNumber++)
+            {
+                var row = workSheet.Cells[rowNumber, 1, rowNumber, workSheet.Dimension.End.Column];
+                var newRow = Dt.NewRow();
+                foreach (var cell in row)
+                {
+                    newRow[cell.Start.Column - 1] = cell.Text;
+                }
+                Dt.Rows.Add(newRow);
+            }
+            return Dt;
+        }
+    }
+}
